19ee1ad4e24b8299fc2b37f39dd3bd2abf1f5265
KelzRue19 <kellyrudochiwarawara@gmail.com>
2023-09-07 23:01:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is my main function
+ * Return: successful 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
