735c8c98a401e2985f734ec83d7e65a011abbe2e
KelzRue19 <kellyrudochiwarawara@gmail.com>
2023-10-10 07:46:54 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 {
 char *k = malloc(size);
 if (size == 0 || k == 0)
-	return (0);
+return (0);
 
 while (size--)
-	k[size] = c;
+k[size] = c;
 
 return (k);
 }
